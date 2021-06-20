0cc9ce386eee1d315bb5f62f53cda91eed88ea04
Groza Marius <groza_marius44@yahoo.com>
2021-06-20 20:55:50 +0300

    modificari

--- src/main/java/fasstrackit/curs17homework/ExercitiiLambdas.java
+++ src/main/java/fasstrackit/curs17homework/ExercitiiLambdas.java
@@ -1,175 +1,127 @@
 package fasstrackit.curs17homework;
 
-import fasstrackit.curs17.TryStreams;
-
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class ExercitiiLambdas {
-    public static List<Person> persons;
+    List<Person> persons=List.of(
+            new Person("Ciurescu", "Ana", 32, "Braila"),
+            new Person("Blodor", "Mihai", 22, "Oradea"),
+            new Person("Petrica", "George", 12, "Oradea"),
+            new Person("Petrut", "Laurentiu", 52, "Cluj"),
+            new Person("Pusca", "Ecaterina", 32, "Cluj"),
+            new Person("Puscas", "Darius", 26, "Oradea"),
+            new Person("Puscas", "Darius", 64, "Oradea"),
+            new Person("Puscas", "Arius", 29, "Oradea"),
+            new Person("Anton", "Crina", 33, "Oradea")
+    );
 
-    public static void listPersons() {
-        persons=List.of(
-                new Person("Ciurescu", "Ana", 32, "Braila"),
-                new Person("Blodor", "Mihai", 22, "Oradea"),
-                new Person("Petrica", "George", 12, "Oradea"),
-                new Person("Petrut", "Laurentiu", 52, "Cluj"),
-                new Person("Pusca", "Ecaterina", 32, "Cluj"),
-                new Person("Puscas", "Darius", 26, "Oradea"),
-                new Person("Puscas", "Darius", 64, "Oradea"),
-                new Person("Puscas", "Arius", 29, "Oradea"),
-                new Person("Anton", "Crina", 33, "Oradea")
-        );
-    }
 
-    public static List<String> ex1() {
+    public List<String> ex1() {
         List<String> names=persons.stream()
                 .map(Person::getFullName)
                 .collect(Collectors.toList());
 
-        System.out.println("Print full names: " + names);
-
         return names;
     }
 
-    public static List<Person> ex2() {
+    public List<Person> ex2() {
         List<Person> major=persons.stream()
                 .filter(person -> person.getAge() >= 18)
                 .collect(Collectors.toList());
 
-        System.out.println("Major persons: " + major);
-
         return major;
     }
 
-    public static List<Person> ex3() {
+    public List<Person> ex3() {
         List<Person> fromOradea=persons.stream()
-                .filter(person -> person.getCity() == "Oradea")
+                .filter(person -> person.getCity().equals("Oradea"))
                 .collect(Collectors.toList());
-
-        System.out.println("From Oradea: " + fromOradea);
 
         return fromOradea;
     }
 
-    public static List<Person> ex4() {
+    public List<Person> ex4() {
         List<Person> fromOradeaorCluj=persons.stream()
-                .filter(person -> person.getCity() == "Oradea" || person.getCity() == "Cluj")
+                .filter(person -> person.getCity().equals("Oradea") || person.getCity().equals("Cluj"))
                 .collect(Collectors.toList());
-
-        System.out.println("From Oradea or Cluj: " + fromOradeaorCluj);
 
         return fromOradeaorCluj;
     }
 
-    public static List<String> ex5() {
-        List<String> CAPITALIZED=persons.stream()
+    public List<String> ex5() {
+        List<String> capitalized=persons.stream()
                 .map(Person::getFirstName)
                 .map(name -> name.toUpperCase())
                 .collect(Collectors.toList());
 
-        System.out.println("First name to uppercase: " + CAPITALIZED);
-
-        return CAPITALIZED;
+        return capitalized;
     }
 
-    public static List<String> ex6() {
+    public List<String> ex6() {
         List<String> firstNamefirstLetter=persons.stream()
                 .map(person -> person.getFirstName() + " " + person.getLastName().charAt(0) + ".")
                 .collect(Collectors.toList());
 
-        System.out.println("first name first letter: " + firstNamefirstLetter);
-
         return firstNamefirstLetter;
     }
 
-    public static List<Person> ex7() {
+    public List<Person> ex7() {
         List<Person> ageBetween=persons.stream()
                 .filter(person -> person.getAge() > 18)
                 .filter(person -> person.getAge() < 60)
                 .collect(Collectors.toList());
 
-
-        System.out.println("Age between 18-60: " + ageBetween);
-
         return ageBetween;
     }
 
-    public static List<Person> ex8() {
+    public List<Person> ex8() {
         List<Person> firstA=persons.stream()
                 .filter(person -> person.getFirstName().charAt(0) == 'A')
                 .collect(Collectors.toList());
 
-
-        System.out.println("First name starting with A: " + firstA);
-
         return firstA;
     }
 
-    public static List<String> ex9() {
+    public List<String> ex9() {
         List<String> uniquely=persons.stream()
-                .map(person -> person.getFirstName())
+                .map(Person::getFirstName)
                 .distinct()
                 .collect(Collectors.toList());
-
-        System.out.println("Uniquely names: " + uniquely);
 
         return uniquely;
     }
 
-    public static List<Person> ex10() {
+    public List<Person> ex10() {
         List<Person> sorting=persons.stream()
                 .sorted(Comparator.comparing(Person::getFirstName))
                 .collect(Collectors.toList());
 
-        System.out.println("Sort by first name: " + sorting);
-
         return sorting;
     }
 
-    public static List<Person> ex11() {
+    public List<Person> ex11() {
         List<Person> sorting=persons.stream()
                 .sorted(Comparator.comparing(Person::getLastName))
                 .collect(Collectors.toList());
 
-        System.out.println("Sort by last name " + sorting);
-
         return sorting;
     }
 
-    public static List<Person> ex12() {
+    public List<Person> ex12() {
         List<Person> sorting=persons.stream()
                 .sorted(Comparator.comparing(Person::getFirstName)
                         .thenComparing(Person::getLastName)
                         .thenComparingInt(Person::getAge))
                 .collect(Collectors.toList());
 
-        System.out.println("Sort by first name,last name,age: " + sorting);
-
         return sorting;
     }
 
-    public static void main(String[] args) {
-        listPersons();
 
-        ex1();
-        ex2();
-        ex3();
-        ex4();
-        ex5();
-        ex6();
-        ex7();
-        ex8();
-        ex9();
-        ex10();
-        ex11();
-        ex12();
-    }
-
-
-  public   static class Person {
+    static class Person {
         private final String firstName;
         private final String lastName;
         private final int    age;
--- src/main/java/fasstrackit/curs17homework/Main.java
+++ src/main/java/fasstrackit/curs17homework/Main.java
@@ -1,2 +1,33 @@
-package fasstrackit.curs17homework;public class Main {
+package fasstrackit.curs17homework;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ExercitiiLambdas ex=new ExercitiiLambdas();
+
+        System.out.println("Print full names: " + ex.ex1());
+
+        System.out.println("Major persons: " + ex.ex2());
+
+        System.out.println("From Oradea: " + ex.ex3());
+
+        System.out.println("From Oradea or Cluj: " + ex.ex4());
+
+        System.out.println("First name to uppercase: " + ex.ex5());
+
+        System.out.println("first name first letter: " + ex.ex6());
+
+        System.out.println("Age between 18-60: " + ex.ex7());
+
+        System.out.println("First name starting with A: " + ex.ex8());
+
+        System.out.println("Uniquely names: " + ex.ex9());
+
+        System.out.println("Sort by first name: " + ex.ex10());
+
+        System.out.println("Sort by last name " + ex.ex11());
+
+        System.out.println("Sort by first name,last name,age: " + ex.ex12());
+
+    }
 }
